aceba26e3cb5a532941df50ca307fe24b34e15e2
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-02-26 12:19:34 -0300

    added secret token for now because of heroku problems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VerkkoCrm::Application.config.secret_token = '54c656670cbf30f05160b0993de3b6a74b6bf2ccbd1cd77c5a52316b0d2616f33e67ad97bd56af3ac6352d8e4b94904cccbb166889b8a48b8a01ae10d6628a34'
