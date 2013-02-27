c367aa65dc66d8a99ebedf323160b552b687cb04
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-02-27 11:26:12 -0300

    fixed prospect active menu bug

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,4 +1,10 @@
 module PagesHelper
+  def active_controller(cntrl_name)
+    if cntrl_name == controller.controller_name
+      return 'active'
+    end
+  end
+
   def active(cntrl_name)
     if cntrl_name == controller.controller_name + '#' + controller.action_name then
       return 'active'
